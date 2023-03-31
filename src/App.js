5c50e7d54ff09a11637d158efdcea6c77e046bb8
Lipika Mohanty <lipikamohanty941@gmail.com>
2023-03-31 12:24:25 +0530

    xyz

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [projects, setProjects] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const [projectName, setProjectName] = useState('');
+  const [taskName, setTaskName] = useState('');
+
+  const handleProjectSubmit = (e) => {
+    e.preventDefault();
+    setProjects([...projects, projectName]);
+    setProjectName('');
+  };
+
+  const handleTaskSubmit = (e) => {
+    e.preventDefault();
+    const newTask = { name: taskName, startTime: new Date() };
+    setTasks([...tasks, newTask]);
+    setTaskName('');
+  };
+
+  const handleTaskStop = (taskIndex) => {
+    const endTime = new Date();
+    const updatedTasks = [...tasks];
+    updatedTasks[taskIndex].endTime = endTime;
+    updatedTasks[taskIndex].timeSpent = endTime - updatedTasks[taskIndex].startTime;
+    setTasks(updatedTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Create a project</h2>
+      <form onSubmit={handleProjectSubmit}>
+        <input
+          type="text"
+          value={projectName}
+          onChange={(e) => setProjectName(e.target.value)}
+          placeholder="Project Name"
+        />
+        <button type="submit">Add Project</button>
+      </form>
+      <h2>Create a task</h2>
+      <form onSubmit={handleTaskSubmit}>
+        <input
+          type="text"
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
+          placeholder="Task Name"
+        />
+        <button type="submit">Add Task</button>
+      </form>
+      <h2>Projects</h2>
+      <ul>
+        {projects.map((project) => (
+          <li key={project}>{project}</li>
+        ))}
+      </ul>
+      <h2>Tasks</h2>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task.name}
+            {task.endTime ? (
+              <span> Time spent: {task.timeSpent / 1000} seconds</span>
+            ) : (
+              <button onClick={() => handleTaskStop(index)}>Stop</button>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
